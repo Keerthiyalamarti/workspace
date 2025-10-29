fe1d76d1d148b0b6180bd2194ba5ccd348b51f94
Keerthiyalamarti <yalamarthikeerthi157@gmail.com>
2025-10-29 10:55:05 +0530

    Add files via upload

---
+++ 4_operator.py
@@ -0,0 +1,34 @@
+num1 = 10
+num2 = 20
+sum = num1 + num2
+sum = num1 + num2
+print("Addition is: ", sum)
+
+#10 = 30
+#print(10)
+
+num1 = 10
+print(num1)
+num1 = 30
+print(num1)
+
+print(num1 + num2)
+
+#arithmatic operators: +, -, -----
+
+num1 = 200
+num2 = 500
+
+sum = num1 + num2 #700
+sub = num1 - num2 #-300
+mul = num1 * num2 #10000
+div = num1 / num2 #5.0
+mod = num2 % num1 #100 #remainder
+print("Sum: ",sum)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
+print("Modulus: ", mod)
+
+floor = num2 // num1 #2
+print("Floor division: ", floor)
